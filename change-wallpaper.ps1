8b0bd82f2d8bdfd2a4a4ea8264f1ed015d46e43a
matthieuclam <matthieu.dufon@gmail.com>
2022-09-09 11:10:32 +0200

    Pushing old project to Github

---
+++ change-wallpaper.ps1
@@ -0,0 +1,23 @@
+function Set-Wallpaper($MyWallpaper){
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code 
+[Win32.Wallpaper]::SetWallpaper($MyWallpaper)
+}
+
+$wpFolder = 'wallpapers'
+$wallpaperToApply = Get-ChildItem $wpFolder | Get-Random | ForEach-Object {$_.FullName}
+Set-Wallpaper($wallpaperToApply)
